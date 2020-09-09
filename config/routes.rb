2a3d9df9f3edb64b94b94bccc9ff869a0c419d58
EC2 Default User <ec2-user@ip-172-31-72-178.ec2.internal>
2020-09-09 11:16:45 +0000

    Update books#index追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+get 'books/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   resources :books
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
 end
