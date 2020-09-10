5550207234e2af8c6cd9c4a47bc0b063e16584f3
EC2 Default User <ec2-user@ip-172-31-72-178.ec2.internal>
2020-09-10 12:40:18 +0000

    Update create他多数

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def new
@@ -10,8 +15,44 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book), notice: 'Book was successfully created.'
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html{ redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json{ head :no_content }
+   end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def set_book
+   @book = Book.find(params[:id])
+  end
+
+
+
 end
