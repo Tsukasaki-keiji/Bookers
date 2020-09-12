809211c04b5dbb19adcc9d902325adbea34bf78c
EC2 Default User <ec2-user@ip-172-31-72-178.ec2.internal>
2020-09-12 05:37:33 +0000

    Fix root to:追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-get 'books/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  root to: 'homes#top'
+
   resources :books
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
 end
